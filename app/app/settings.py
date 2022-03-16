c566a6386cfe38fc7ab2a6d20f51967ec8690594
hamza <hamza@codeplumbers.eu>
2022-03-16 15:11:34 +0100

    feat(app): added env var file

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(find_dotenv())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n87lx)p^+3hdpexu4e#4=(bh*3k4-mhud*@343mc7pwbg@&j1b'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
